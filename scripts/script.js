5327ca9d78afee494f2d4460d58cb96433224feb
Naimur Rahman Niloy <niloyrahman432@gmail.com>
2026-03-09 06:47:18 +0600

    displaying the issue cards

--- scripts/script.js
+++ scripts/script.js
@@ -24,3 +24,80 @@ const loadAllIssuesCard = async () => {
   displayCard(data.data);
   loading(false);
 };
+
+// displaying cards data
+const displayCard = (data) => {
+  // dynamic cards count
+  cardsCount.innerText = "";
+  cardsCount.innerText = data.length;
+  // created new element and append it
+  issueCards.innerHTML = "";
+  data.forEach((card) => {
+    const div = document.createElement("div");
+    div.id = `border-${card.id}`;
+    div.className =
+      "card bg-base-100 max-w-96 shadow-sm border-t-4 border-green-500 h-full";
+    div.innerHTML = `
+         <div onclick="modal(${card.id})" class="card-body">
+
+                        <div class="flex justify-between">
+                            <img src=${card.status === "open" ? "./assets/Open-Status.png" : "./assets/clonse.png"} alt="">
+                            <div id="card-${card.id}" class="badge font-semibold bg-[#FEECEC] text-red-500">${card.priority.toUpperCase()}</div>
+                        </div>
+                        <h2 class="card-title">${card.title}</h2>
+                        <p class="text-[#64748B]">${card.description}</p>
+                            <div class="flex items-center flex-wrap gap-2" id="badgeParent-${card.id}">
+                        </div>
+                        <div class="card-actions justify-end border-t-2 border-gray-200 flex flex-col gap-1 mt-4 pt-5">
+                            <p class="text-[#64748B] text-sm">${card.author}</p>
+                            <p class="text-[#64748B] text-sm">${card.createdAt}</p>
+                        </div>
+                    </div>
+        `;
+    issueCards.appendChild(div);
+    const badgeParent = document.getElementById(`badgeParent-${card.id}`);
+    badgeParent.innerHTML = "";
+    card.labels.forEach((l) => {
+      const badge = document.createElement("div");
+      if (l === "bug") {
+        badge.innerHTML = `
+                                    <div class="badge bg-[#FEECEC] text-red-500"><img src="./assets/BugDroid.png" alt="">${l.toUpperCase()}
+                            </div>
+                                    `;
+      } else if (l === "enhancement") {
+        badge.innerHTML = `
+            <div class="badge  bg-green-100 text-green-600"><i class="fa-solid fa-wand-magic-sparkles"></i><span>${l.toUpperCase()}</span></div>
+            `;
+      } else if (l === "help wanted") {
+        badge.innerHTML = `
+                <div class="badge bg-[#FFF8DB] text-[#D97706]"><img src="./assets/Lifebuoy.png" alt="">${l.toUpperCase()}</div>
+                `;
+      } else {
+        badge.innerHTML = `
+            <div class="badge  bg-gray-100 text-gray-600"><i class="fa-solid fa-circle-exclamation"></i><span>${l.toUpperCase()}</span></div>
+            `;
+      }
+      badgeParent.appendChild(badge);
+    });
+
+    // change border color depends on status
+    const borderTop = document.getElementById(`border-${card.id}`);
+    if (card.status === "closed") {
+      borderTop.classList.remove("border-green-500");
+      borderTop.classList.add("border-[#A855F7]");
+    }
+
+    // dynamic priority set
+    const dynamicPriority = document.getElementById(`card-${card.id}`);
+    if (card.priority === "high") {
+      dynamicPriority.classList.add("text-red-500", "bg-[#FEECEC]");
+      dynamicPriority.classList.remove("bg-[#FFF8DB]", "text-[#D97706]");
+    } else if (card.priority === "low") {
+      dynamicPriority.classList.remove("text-red-500", "bg-[#FEECEC]");
+      dynamicPriority.classList.add("bg-gray-100", "text-gray-500");
+    } else {
+      dynamicPriority.classList.remove("text-red-500", "bg-[#FEECEC]");
+      dynamicPriority.classList.add("bg-[#FFF8DB]", "text-[#D97706]");
+    }
+  });
+};
